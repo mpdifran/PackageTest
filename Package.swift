2fcf6daf17002d3434ea32a0b721e6d37226d943
Mark DiFranco <mpdifranco@icloud.com>
2019-10-08 21:04:29 -0400

    Update to force dynamic library

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v3)
     ],
     products: [
-        .library(name: "PackageTest", targets: ["PackageTest"])
+        .library(name: "PackageTest", type: .dynamic, targets: ["PackageTest"])
     ],
     dependencies: [
         .package(url: "git@github.com:Swinject/Swinject.git", from: "2.7.0")
